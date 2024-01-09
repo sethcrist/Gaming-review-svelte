c9d5892b1b36b0e60baeab8f9e2e057b53637978
Seth Crist <sethcrist50@gmail.com>
2024-01-09 16:38:13 -0600

    added reusable components for the title

---
+++ src/lib/title/+page.svelte
@@ -0,0 +1,27 @@
+<script>
+    export let title = "Korvo Gaming"
+</script>
+
+<div class="title">
+    <img src="/gaming.jpg" alt="site icon">
+    <h1>{title}</h1>
+</div>
+
+<style>
+    .title {
+        display: flex;
+    }
+    .title img {
+        display: inline-block;
+
+    }
+    .title h1 {
+        margin-left: 10px;
+    }
+    h1 {
+        display: inline-block;
+        margin-top: 57px;
+        font-size: 3em;
+        /*color: linear-gradient(to right, blue, purple)*/
+    }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,26 +1,17 @@
 <script>
-    let title = "Gaming Guide for the inept."
-
-    const updateTitle = () => {
-        title = "something different"
-    }
+    import Title from "$lib/title/+page.svelte"
 </script>
 
+
+<Title />
 <div class="index">
-    <h2>{title}</h2>
+    <h2>Welcome</h2>
     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam asperiores aspernatur assumenda autem,
         delectus dicta doloribus ducimus eius enim eveniet facere fugiat illo impedit iste laboriosam laborum magnam
         natus nobis non perspiciatis quae qui quo recusandae rem tempora totam ut vel veniam vero voluptas. Eligendi,
         ipsa, nihil! Accusantium assumenda delectus eum ipsa necessitatibus nulla pariatur, quos, tempore tenetur unde vero.</p>
-
-    <button on:click={updateTitle}>Change title</button>
-
-<!--binding values to input fields-->
-    <input type="text" bind:value={title}>
-
     <a href="/guides">View Guides</a>
     <a href="/about">About</a>
-
 </div>
 
 <style>
