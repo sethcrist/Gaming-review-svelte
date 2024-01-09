13c95f6d556190a214fc44d6b3a681b1758662e3
Seth Crist <sethcrist50@gmail.com>
2024-01-09 13:14:45 -0600

    learned some svelte components

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,29 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    let title = "Gaming Guide for the inept."
+
+    const updateTitle = () => {
+        title = "something different"
+    }
+</script>
+
+<div class="index">
+    <h2>{title}</h2>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam asperiores aspernatur assumenda autem,
+        delectus dicta doloribus ducimus eius enim eveniet facere fugiat illo impedit iste laboriosam laborum magnam
+        natus nobis non perspiciatis quae qui quo recusandae rem tempora totam ut vel veniam vero voluptas. Eligendi,
+        ipsa, nihil! Accusantium assumenda delectus eum ipsa necessitatibus nulla pariatur, quos, tempore tenetur unde vero.</p>
+
+    <button on:click={updateTitle}>Change title</button>
+
+<!--binding values to input fields-->
+    <input type="text" bind:value={title}>
+</div>
+
+<style>
+    .index {
+        text-align: center;
+        display: block;
+        margin: 20px auto;
+    }
+
+</style>
