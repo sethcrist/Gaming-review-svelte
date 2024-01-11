f7d4f5ec438ce0c7d6e0402a44b8ea3d70be70d1
Seth Crist <sethcrist50@gmail.com>
2024-01-11 13:30:19 -0600

    tried to access the data, no luck

--- src/routes/guides/[id]/+page.svelte
+++ src/routes/guides/[id]/+page.svelte
@@ -1,4 +1,3 @@
-<!--fetching data-->
 <script>
     import {onMount} from "svelte";
 
@@ -19,10 +18,11 @@
 
     /** @type {Guide[]} */
     let guides = [];
+    let page = 1;
 
     onMount(async () => {
         /** @type {Response} */
-        const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+        const res = await fetch(`https://jsonplaceholder.typicode.com/posts?_page=${page}`);
         if (res.ok) {
             guides = await res.json();
             console.log(res);
@@ -30,31 +30,15 @@
     });
 </script>
 
-<div class="guides">
-    <ul>
-        {#each guides as guide}
-           <li>
-               <a href={`/guides/[id]`}>{guide.title}</a>
-           </li>
-        {/each}
-    </ul>
+<div class="guide">
+    <h2>{guides.title}</h2>
+    <p>{guides.body}</p>
 </div>
 
-<!--<footer>-->
-<!--</footer>-->
-
 <style>
-    .guides {
-        margin-top: 20px;
-    }
-    ul {
-        list-style-type: none;
-        padding: 0;
-    }
-    a {
-        display: inline-block;
-        margin-top: 10px;
+    .guide {
+        margin-top: 40px;
         padding: 10px;
-        border: 1px dotted rgba(255, 255, 255, 0.2);
+        border: 1px dotted rgba(255,255,255,0.2);
     }
 </style>
